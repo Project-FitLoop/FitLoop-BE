8d88f29c385b45490fefd8a6af6e06010989b588
Jieun-KWAK <jieun47631@gmail.com>
2025-01-03 22:42:24 +0900

    setting

---
+++ src/main/java/fitloop/FitloopApplication.java
@@ -0,0 +1,13 @@
+package fitloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitloopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitloopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fitloop/FitloopApplicationTests.java
@@ -0,0 +1,13 @@
+package fitloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitloopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
