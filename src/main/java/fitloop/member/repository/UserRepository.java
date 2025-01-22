f8087e5389e49672f6499229ce5b726b93fe41c7
Jieun-KWAK <jieun47631@gmail.com>
2025-01-23 02:44:15 +0900

    [feature/#3] UserRepository 인터페이스 구현

---
+++ src/main/java/fitloop/member/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package fitloop.member.repository;
+
+import fitloop.member.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Boolean existsByUsername(String username);
+}
