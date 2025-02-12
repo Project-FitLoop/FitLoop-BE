d743a2fb9baa0379e11085f347a805ca83ca1ecf
Jieun-KWAK <jieun47631@gmail.com>
2025-02-13 05:02:03 +0900

    [refactor/#8] 회원가입 DTO 수정

--- src/main/java/fitloop/member/dto/request/RegisterRequest.java
+++ src/main/java/fitloop/member/dto/request/RegisterRequest.java
@@ -1,9 +1,12 @@
 package fitloop.member.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -16,4 +19,11 @@ public class RegisterRequest {
     @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
     @Size(min = 6, message = "비밀번호는 최소 6자 이상이어야 합니다.")
     private String password;
+
+    @NotBlank(message = "이름은 필수 입력 항목입니다.")
+    @Size(min = 2, max = 50, message = "이름은 2~50자 사이여야 합니다.")
+    private String name;
+
+    @Past(message = "생일은 과거 날짜여야 합니다.")
+    private LocalDate birthday;
 }
