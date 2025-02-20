2530ad0f38a66775bf8b4c19f330277a56087335
LouiIII3 <jikdsunghuilee@naver.com>
2025-02-20 23:34:19 +0900

    [feat/#11] RegisterService - 회원가입 로직 추가 - 회원가입 요청을 처리하는 registerProcess 메서드 구현 - 사용자명 또는 이메일 중복 체크 로직 추가 - 비밀번호 암호화 처리 (BCryptPasswordEncoder 사용) - 새 사용자 정보를 UserRepository를 통해 저장

--- src/main/java/fitloop/member/service/RegisterService.java
+++ src/main/java/fitloop/member/service/RegisterService.java
@@ -3,6 +3,7 @@ package fitloop.member.service;
 import fitloop.member.dto.request.RegisterRequest;
 import fitloop.member.entity.UserEntity;
 import fitloop.member.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,22 +17,25 @@ public class RegisterService {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
-    public void registerProcess(RegisterRequest registerRequest) {
+    @Transactional
+    public UserEntity registerProcess(RegisterRequest registerRequest) {
         String username = registerRequest.getUsername();
         String password = registerRequest.getPassword();
+        String email = registerRequest.getEmail();
 
-        Boolean isExist = userRepository.existsByUsername(username);
-
-        if (isExist) {
-            return;
+        // 사용자 이름 또는 이메일이 이미 존재하는지 확인
+        if (userRepository.existsByUsername(username) || userRepository.existsByEmail(email)) {
+            throw new IllegalArgumentException("이미 존재하는 사용자입니다.");
         }
 
-        UserEntity data = UserEntity.builder()
+        // 새 사용자 생성
+        UserEntity user = UserEntity.builder()
                 .username(username)
                 .password(bCryptPasswordEncoder.encode(password))
+                .email(email)
                 .birthday(registerRequest.getBirthday())
                 .build();
 
-        userRepository.save(data);
+        return userRepository.save(user);
     }
 }
