a0780341506a9961b79ae6ef1c0d462177c28f20
LouiIII3 <jikdsunghuilee@naver.com>
2025-02-20 11:31:16 +0900

    [feat/#11] GlobalExceptionHandler - 전역 예외 처리 추가 - NoHandlerFoundException (404) 처리 - MethodArgumentNotValidException (400) 처리 - HttpRequestMethodNotSupportedException (405) 처리 - 기타 예외 (500) 처리

---
+++ src/main/java/fitloop/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,51 @@
+package fitloop.common.exception;
+
+
+import fitloop.common.exception.errorcode.CommonErrorCode;
+import fitloop.common.exception.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NoHandlerFoundException ex) {
+        String requestUrl = ex.getRequestURL(); // 요청한 URL 가져오기
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ErrorResponse.of(CommonErrorCode.RESOURCE_NOT_FOUND, requestUrl)); //
+    }
+
+
+    // 400 Bad Request (잘못된 입력값)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse.of(CommonErrorCode.INVALID_INPUT, errors.toString()));
+    }
+
+    // 405 Method Not Allowed (잘못된 HTTP 메서드 요청)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleMethodNotAllowedException(HttpRequestMethodNotSupportedException ex) {
+        String method = ex.getMethod(); // 요청된 HTTP 메서드 (예: POST)
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(ErrorResponse.of(CommonErrorCode.HTTP_METHOD_NOT_ALLOWED, method));
+    }
+
+    // 500 Internal Server Error (서버 내부 오류)
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ErrorResponse.from(CommonErrorCode.INTERNAL_SERVER_ERROR));
+    }
+}
