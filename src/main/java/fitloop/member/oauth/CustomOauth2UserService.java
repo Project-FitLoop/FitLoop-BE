972b9e9d33e7945a25f7d32f55073d27e1f60a05
LouiIII3 <jikdsunghuilee@naver.com>
2025-02-20 23:44:11 +0900

    [feat/#11] CustomOauth2UserService - OAuth2 사용자 서비스 추가 - Google OAuth2 로그인 사용자 정보를 가져와 인증 처리 - OAuth2UserInfo 인터페이스를 활용하여 사용자 정보 매핑

---
+++ src/main/java/fitloop/member/oauth/CustomOauth2UserService.java
@@ -0,0 +1,62 @@
+package fitloop.member.oauth;
+
+import fitloop.member.dto.request.CustomUserDetails;
+import fitloop.member.entity.Membership;
+import fitloop.member.entity.Role;
+import fitloop.member.entity.UserEntity;
+import fitloop.member.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CustomOauth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+        OAuth2UserInfo oAuth2UserInfo = null;
+
+        if ("google".equals(provider)) {
+            oAuth2UserInfo = new GoogleUserDetails(oAuth2User.getAttributes());
+        }
+
+        if (oAuth2UserInfo == null) {
+            throw new OAuth2AuthenticationException("OAuth2UserInfo is null");
+        }
+
+        String providerId = oAuth2UserInfo.getProviderId();
+        String email = oAuth2UserInfo.getEmail();
+        String name = oAuth2UserInfo.getName();
+
+        //사용자 조회 후 없으면 저장
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseGet(() -> {
+                    UserEntity newUser = UserEntity.builder()
+                            .username("유저") //기본값으로 유저 등록 나중에 바꿔야 함
+                            .email(email)
+                            .provider(provider)
+                            .providerId(providerId)
+                            .role(Role.MEMBER)
+                            .membership(Membership.LEAF) // 기본 membership 설정
+                            .password("") // OAuth 로그인은 비밀번호 X
+                            .phoneNumber("") // 기본 전화번호 없음
+                            .personalInfo(false) // 개인정보 동의 기본값
+                            .build();
+                    return userRepository.save(newUser);
+                });
+
+        return new CustomUserDetails(user, oAuth2User.getAttributes());
+    }
+}
