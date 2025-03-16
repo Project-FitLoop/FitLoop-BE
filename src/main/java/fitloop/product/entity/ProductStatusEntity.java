b5c194e48cae69691368fbeb51b9fe5d7ae216b4
Jieun-KWAK <jieun47631@gmail.com>
2025-03-16 22:39:13 +0900

    [feat/#17] 상품 상태 엔티티 작성

---
+++ src/main/java/fitloop/product/entity/ProductStatusEntity.java
@@ -0,0 +1,28 @@
+package fitloop.product.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product_status")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductStatusEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String name;
+
+    @Column(nullable = false, length = 255)
+    private String condition;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
