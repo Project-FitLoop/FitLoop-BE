647e426a7ffd2ebc251f0e2eb949fe8a72acd432
Jieun-KWAK <jieun47631@gmail.com>
2025-03-24 20:44:53 +0900

    [feat/#17] ProductRegisterRequest 작성 및 Enum 타입 변환 메소드 추가

--- src/main/java/fitloop/product/dto/request/ProductRegisterRequest.java
+++ src/main/java/fitloop/product/dto/request/ProductRegisterRequest.java
@@ -1,9 +1,7 @@
 package fitloop.product.dto.request;
 
 import fitloop.product.entity.ProductConditionCategory;
-import fitloop.product.entity.category.BottomCategory;
-import fitloop.product.entity.category.MiddleCategory;
-import fitloop.product.entity.category.TopCategory;
+import fitloop.product.entity.category.*;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,15 +11,14 @@ import java.util.List;
 @Getter
 @Setter
 public class ProductRegisterRequest {
-
     @NotBlank(message = "상품명은 필수 입력값입니다.")
     private String productName;
 
     @NotBlank(message = "카테고리는 필수 입력값입니다.")
-    private MiddleCategory category;
+    private String category;
 
     @NotBlank(message = "서브 카테고리는 필수 입력값입니다.")
-    private BottomCategory subCategory;
+    private String subCategory;
 
     @Min(value = 0, message = "가격은 0 이상이어야 합니다.")
     private int price;
@@ -30,17 +27,33 @@ public class ProductRegisterRequest {
     private boolean isFree;
 
     @NotNull(message = "배송비 포함 여부는 필수 입력값입니다.")
-    private boolean includeShipping;
+    private boolean inCludeShipping;
 
     @NotEmpty(message = "상품 이미지는 최소 1개 이상 필요합니다.")
     private List<String> images;
 
     @NotBlank(message = "상품 상태는 필수 입력값입니다.")
-    private ProductConditionCategory productCondition;
+    private String productCondition;
 
     @Size(max = 500, message = "상품 설명은 500자 이내여야 합니다.")
     private String productDescription;
 
     @NotBlank(message = "성별 선택은 필수 입력값입니다.")
-    private TopCategory gender;
+    private String gender;
+
+    public TopCategory getTopCategoryEnum() {
+        return TopCategory.from(this.gender);
+    }
+
+    public MiddleCategory getMiddleCategoryEnum() {
+        return MiddleCategory.from(this.category);
+    }
+
+    public BottomCategory getBottomCategoryEnum() {
+        return BottomCategory.from(this.subCategory);
+    }
+
+    public ProductConditionCategory getProductConditionEnum() {
+        return ProductConditionCategory.from(this.productCondition);
+    }
 }
