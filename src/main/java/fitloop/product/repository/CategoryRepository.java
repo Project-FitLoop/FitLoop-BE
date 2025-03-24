87bfdbe037fddbd0aa5b9d5bec1ecc7ba122a5b5
Jieun-KWAK <jieun47631@gmail.com>
2025-03-24 21:11:07 +0900

    [feat/#17] 사용하지 않는 import문 제거

--- src/main/java/fitloop/product/repository/CategoryRepository.java
+++ src/main/java/fitloop/product/repository/CategoryRepository.java
@@ -1,13 +1,8 @@
 package fitloop.product.repository;
 
 import fitloop.product.entity.CategoryEntity;
-import fitloop.product.entity.category.BottomCategory;
-import fitloop.product.entity.category.MiddleCategory;
-import fitloop.product.entity.category.TopCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
--- src/main/java/fitloop/product/repository/ProductConditionRepository.java
+++ src/main/java/fitloop/product/repository/ProductConditionRepository.java
@@ -1,11 +1,8 @@
 package fitloop.product.repository;
 
-import fitloop.product.entity.ProductConditionCategory;
 import fitloop.product.entity.ProductConditionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface ProductConditionRepository extends JpaRepository<ProductConditionEntity, Long> {
