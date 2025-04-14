09dc386a7db5344d3f46f5c20e1a83584c3c688f
LouiIII3 <jikdsunghuilee@naver.com>
2025-04-14 16:12:01 +0900

    [feat/#23] ProductTagRepository에 다중 상품 ID 기반 태그 조회 메서드 추가
    - 상품 ID 목록으로 해당 상품들의 태그를 일괄 조회할 수 있도록
      findByProductEntityIdIn(List<Long> productIds) 메서드 추가

--- src/main/java/fitloop/product/repository/ProductTagRepository.java
+++ src/main/java/fitloop/product/repository/ProductTagRepository.java
@@ -4,6 +4,9 @@ import fitloop.product.entity.ProductTagEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductTagRepository extends JpaRepository<ProductTagEntity, Long> {
+    List<ProductTagEntity> findByProductEntityIdIn(List<Long> productIds);
 }
