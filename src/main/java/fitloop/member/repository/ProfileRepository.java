6a1f327f844920f4abde622043d51a2d6835abd7
LouiIII3 <jikdsunghuilee@naver.com>
2025-05-05 10:43:34 +0900

    [feat/#27] ProfileRepository에 사용자 정보 조회 메서드 추가

--- src/main/java/fitloop/member/repository/ProfileRepository.java
+++ src/main/java/fitloop/member/repository/ProfileRepository.java
@@ -1,9 +1,11 @@
 package fitloop.member.repository;
 
 import fitloop.member.entity.ProfileEntity;
+import fitloop.member.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface ProfileRepository extends JpaRepository<ProfileEntity, Long> {
+    Optional<ProfileEntity> findByUserId(UserEntity userEntity);
 }
