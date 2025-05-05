07cad55491a1775680399c03f22662d5fce0f3a8
LouiIII3 <jikdsunghuilee@naver.com>
2025-05-05 10:43:22 +0900

    [feat/#27] ProductCategoryRelationRepository에 상세 조회용 메서드 추가

--- src/main/java/fitloop/product/repository/ProductCategoryRelationRepository.java
+++ src/main/java/fitloop/product/repository/ProductCategoryRelationRepository.java
@@ -1,9 +1,11 @@
 package fitloop.product.repository;
 
 import fitloop.product.entity.ProductCategoryRelationEntity;
+import fitloop.product.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface ProductCategoryRelationRepository extends JpaRepository<ProductCategoryRelationEntity, Long> {
+    Optional<ProductCategoryRelationEntity> findByProductEntity(ProductEntity product);
 }
