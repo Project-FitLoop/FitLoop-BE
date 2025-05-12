bfee52322ec3b61ef55597dc19e1106af61c7799
LouiIII3 <jikdsunghuilee@naver.com>
2025-05-13 03:11:34 +0900

    [feat/#27] RefreshRepository에 리프레시 토큰으로 조회하는 findByRefresh 메서드 추가

--- src/main/java/fitloop/member/repository/RefreshRepository.java
+++ src/main/java/fitloop/member/repository/RefreshRepository.java
@@ -4,9 +4,13 @@ import fitloop.member.entity.RefreshEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 public interface RefreshRepository extends JpaRepository<RefreshEntity, Long> {
 
     Boolean existsByRefresh(String refresh);
+
+    Optional<RefreshEntity> findByRefresh(String refresh);
 
     @Transactional
     void deleteByRefresh(String refresh);
