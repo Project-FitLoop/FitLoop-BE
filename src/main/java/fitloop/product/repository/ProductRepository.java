370fb53db7d6151eed8e58057ac9c9559640baf8
Jieun-KWAK <jieun47631@gmail.com>
2025-05-12 14:31:18 +0900

    [feat/#29] ProductRepository에 findByCategory 메서드 추가

--- src/main/java/fitloop/product/repository/ProductRepository.java
+++ src/main/java/fitloop/product/repository/ProductRepository.java
@@ -2,9 +2,14 @@ package fitloop.product.repository;
 
 import fitloop.product.entity.ProductEntity;
 
+import fitloop.product.entity.category.BottomCategory;
+import fitloop.product.entity.category.MiddleCategory;
+import fitloop.product.entity.category.TopCategory;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +17,20 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     Page<ProductEntity> findAllByIsActiveTrue(Pageable pageable);
+
+    @Query("""
+        SELECT p FROM ProductEntity p
+        JOIN ProductCategoryRelationEntity rel ON p.id = rel.productEntity.id
+        JOIN CategoryEntity c ON rel.categoryEntity.id = c.id
+        WHERE c.topCategory = :top
+          AND c.middleCategory = :middle
+          AND c.bottomCategory = :bottom
+          AND p.isActive = true
+        """)
+    Page<ProductEntity> findByCategory(
+            @Param("top") TopCategory top,
+            @Param("middle") MiddleCategory middle,
+            @Param("bottom") BottomCategory bottom,
+            Pageable pageable
+    );
 }
