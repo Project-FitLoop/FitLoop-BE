93a24bc892991c2013443259dcdc6211ed2f2dbb
Jieun-KWAK <jieun47631@gmail.com>
2025-05-14 05:14:33 +0900

    [feat/#29] ProductRepository에 전체 필터를 고려한 조회 쿼리 수정

--- src/main/java/fitloop/product/repository/ProductRepository.java
+++ src/main/java/fitloop/product/repository/ProductRepository.java
@@ -19,15 +19,31 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     Page<ProductEntity> findAllByIsActiveTrue(Pageable pageable);
 
     @Query("""
-        SELECT p FROM ProductEntity p
-        JOIN ProductCategoryRelationEntity rel ON p.id = rel.productEntity.id
-        JOIN CategoryEntity c ON rel.categoryEntity.id = c.id
-        WHERE c.topCategory = :top
-          AND c.middleCategory = :middle
-          AND c.bottomCategory = :bottom
-          AND p.isActive = true
-        """)
-    Page<ProductEntity> findByCategory(
+    SELECT p FROM ProductEntity p
+    JOIN ProductCategoryRelationEntity rel ON p.id = rel.productEntity.id
+    JOIN CategoryEntity c ON rel.categoryEntity.id = c.id
+    WHERE (:top = fitloop.product.entity.category.TopCategory.ALL AND c.topCategory IN (fitloop.product.entity.category.TopCategory.MALE, fitloop.product.entity.category.TopCategory.FEMALE)
+           OR c.topCategory = :top)
+      AND c.middleCategory = :middle
+      AND p.isActive = true
+    """)
+    Page<ProductEntity> findByTopAndMiddle(
+            @Param("top") TopCategory top,
+            @Param("middle") MiddleCategory middle,
+            Pageable pageable
+    );
+
+    @Query("""
+    SELECT p FROM ProductEntity p
+    JOIN ProductCategoryRelationEntity rel ON p.id = rel.productEntity.id
+    JOIN CategoryEntity c ON rel.categoryEntity.id = c.id
+    WHERE (:top = fitloop.product.entity.category.TopCategory.ALL AND c.topCategory IN (fitloop.product.entity.category.TopCategory.MALE, fitloop.product.entity.category.TopCategory.FEMALE)
+           OR c.topCategory = :top)
+      AND c.middleCategory = :middle
+      AND c.bottomCategory = :bottom
+      AND p.isActive = true
+    """)
+    Page<ProductEntity> findByTopMiddleBottom(
             @Param("top") TopCategory top,
             @Param("middle") MiddleCategory middle,
             @Param("bottom") BottomCategory bottom,
