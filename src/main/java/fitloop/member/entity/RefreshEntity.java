d07832dc6179419542f56090139bb5e43a79f976
LouiIII3 <jikdsunghuilee@naver.com>
2025-05-24 21:49:18 +0900

    [fix/#39] RefreshEntity의 만료일 고정 유지

--- src/main/java/fitloop/member/entity/RefreshEntity.java
+++ src/main/java/fitloop/member/entity/RefreshEntity.java
@@ -32,11 +32,11 @@ public class RefreshEntity {
     @Column(nullable = false)
     private LocalDateTime lastReissuedAt;
 
-    public static RefreshEntity createRenewed(String username, String refresh, LocalDateTime expiration, LocalDateTime originalFirstIssuedAt) {
+    public static RefreshEntity createRenewed(String username, String refresh, LocalDateTime originalExpiration, LocalDateTime originalFirstIssuedAt) {
         return RefreshEntity.builder()
                 .username(username)
                 .refresh(refresh)
-                .expiration(expiration)
+                .expiration(originalExpiration)
                 .firstIssuedAt(originalFirstIssuedAt)
                 .lastReissuedAt(LocalDateTime.now())
                 .build();
